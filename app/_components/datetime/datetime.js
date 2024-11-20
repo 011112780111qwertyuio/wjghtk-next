cf65dfe9e7644170625af15bf930c20799f73efc
Ahmed Fathi Abdel Salam <121492048+011112780111qwertyuio@users.noreply.github.com>
2024-11-20 21:44:28 +0200

    Update datetime.js

--- app/_components/datetime/datetime.js
+++ app/_components/datetime/datetime.js
@@ -1,37 +1,43 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './style.scss';
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight, faCalendar } from "@fortawesome/free-solid-svg-icons";
 import LoadingBtn from "../loading-btn/loading-btn";
+
 function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
   const [isOpen, setIsOpen] = useState(false);
   const [savedDt, setSavedDt] = useState([]);
   const dropdownRef = useRef(null);
+  const touchStartX = useRef(0);  // Use ref to store the touch start X position
+  const touchEndX = useRef(0);    // Use ref to store the touch end X position
+
   const toggleDropdown = () => {
     setIsOpen(prevIsOpen => !prevIsOpen);
   };
+
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setIsOpen(false);
     }
   };
+
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
+
   /* show date */
   const [isloading, setloading] = useState(true);
   const [data, setmydata] = useState(null);
   const [selectedDay, setselectedDay] = useState([]);
+
   const funselected = (index) => {
     clearAllData();
     var arr = [];
-    year.toString().padStart('0', 2);
     let res = data.find(person => person.startTime === year + "-" + month + "-" + index);
     if (data.find(person => person.startTime === year + "-" + month + "-" + index && person.state === "on") !== undefined) {
       funItems(res);
@@ -44,37 +50,34 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
       const edit = {
         "startTime": mystar.toString().padStart(2, '0') + "/" + month.toString().padStart(2, '0') + "/" + year,
         "endTime": stop.toString().padStart(2, '0') + "/" + month.toString().padStart(2, '0') + "/" + year,
-
       }
       setSavedDt([edit]);
       setselectedDay(arr);
       toggleDropdown();
-    }
-    else {
+    } else {
       setSavedDt([]);
       setselectedDay([]);
       clearAllData();
     }
-  }
-  /* show date */
+  };
+
   const formation = {
     monthEn: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     monthAr: ["", "يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
-    daysAr: [
-      "س", "أ", "اث", "ث", "أرب", "خ", "ج"
-    ]
-  }
+    daysAr: ["س", "أ", "اث", "ث", "أرب", "خ", "ج"]
+  };
+
   const [dateTime, setdateTime] = useState(new Date());
   const [year, setyear] = useState(dateTime.getFullYear());
   const [month, setmonth] = useState(dateTime.getMonth() + 1);
   const [allDayes, setallDayes] = useState(new Date(year, month + 1, 0).getDate());
+
   const funButton = (btn) => {
     if (btn === "right") {
       if (month === 12) {
         setmonth(1);
         setyear(year + 1);
-      }
-      else {
+      } else {
         setmonth(month + 1);
       }
     }
@@ -82,19 +85,20 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
       if (month === 1) {
         setmonth(12);
         setyear(year - 1);
-      }
-      else {
+      } else {
         setmonth(month - 1);
       }
     }
     fundays(year, month);
     setselectedDay([]);
     setSavedDt([]);
-  }
+  };
+
   const fundays = (isyear, ismonth) => {
     const dt = new Date(isyear, ismonth + 1, 0).getDate();
     setallDayes(dt);
-  }
+  };
+
   var i = 1;
   var elementRows = [];
   const rows = () => {
@@ -106,35 +110,32 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
       )
     }
     return elementRows;
-  }
+  };
+
   const td = () => {
-    let f = + i;
+    let f = +i;
     var element = [];
     if (i <= allDayes) {
       for (let y = 0; y <= 6; y++) {
         if (i <= allDayes) {
           element.push(<td key={y} >
-
             {data == null ?
               <input type="text" readOnly className="disble" value={i} />
               :
               <input onClick={(eo) => { funselected(eo.target.value) }} className={data.find(person => person.startTime === year + "-" + month + "-" + i && person.state === "on") !== undefined && selectedDay.find(person => person === i) === undefined ? "active" : selectedDay.length === 0 && data.find(person => person.startTime === year + "-" + month + "-" + i) === undefined ? "disble" : selectedDay.find(person => person === i) !== undefined && data[0].startTime.split('-')[0] == year && data[0].startTime.split('-')[1] == month ? "selected" : "disble"} type="text" readOnly value={i} />
             }
             <input type="text" defaultValue={i++} hidden />
-
-
-
-
-
           </td>)
         }
       }
     }
     return element;
-  }
+  };
+
   useEffect(() => {
     showdata();
-  }, [year, month])
+  }, [year, month]);
+
   const showdata = async () => {
     setloading(true);
     const edit = year + "-" + month + "-";
@@ -145,21 +146,33 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
     await axios.post(url, obj).then((res) => {
       if (res.data.state === "ok") {
         setmydata(res.data.data);
-      }
-      else {
+      } else {
         setmydata(null);
         clearAllData();
       }
     })
     setloading(false);
+  };
 
-  }
+  // Handle touch events for swipe left/right
+  const handleTouchStart = (e) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
 
+  const handleTouchMove = (e) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
 
-
+  const handleTouchEnd = () => {
+    if (touchStartX.current - touchEndX.current > 50) {
+      funButton("right"); // Swipe left to go to the next month
+    }
+    if (touchEndX.current - touchStartX.current > 50) {
+      funButton("left"); // Swipe right to go to the previous month
+    }
+  };
 
   return (
-
     <div className="dateTime">
       <div onClick={toggleDropdown} className="show-time">
         <FontAwesomeIcon icon={faCalendar} />
@@ -171,13 +184,14 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
       </div>
       {
         isOpen &&
-        <div ref={dropdownRef} className="parent-time show">
+        <div ref={dropdownRef} className="parent-time show"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}>
           <div className="top-parent">
-            <span onClick={() => { funButton("left") }}> <FontAwesomeIcon icon={faAngleRight} /></span>
+            <span onClick={() => { funButton("left") }}><FontAwesomeIcon icon={faAngleRight} /></span>
             <p>{formation.monthAr[month] + " " + year}</p>
-
             <span onClick={() => { funButton("right") }}><FontAwesomeIcon icon={faAngleLeft} /></span>
-
           </div>
           <div className="my-table">
             {isloading === true && data === null &&
@@ -192,23 +206,16 @@ function Datetime({ funItems, uid, local, funDataFormation, clearAllData }) {
                     <th key={index} scope="col">{item}</th>
                   ))}
                 </tr>
-
               </thead>
               <tbody>
-
-
                 {rows()}
-
               </tbody>
             </table>
           </div>
         </div>
       }
     </div>
-
-
-
   );
-
 }
+
 export default React.memo(Datetime);
