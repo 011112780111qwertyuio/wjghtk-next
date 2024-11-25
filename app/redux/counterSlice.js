96d3aa3cfa7b58a8bdcf60f6f6550a3cce063b0c
Ahmed Fathi Abdel Salam <121492048+011112780111qwertyuio@users.noreply.github.com>
2024-11-25 22:15:56 +0200

    Update counterSlice.jsgf

--- app/redux/counterSlice.js
+++ app/redux/counterSlice.js
@@ -1,1 +1,56 @@
 
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+// Create an async thunk for fetching country data
+export const fetchCountryData = createAsyncThunk(
+  'counter/fetchCountryData',
+  async () => {
+    const response = await axios.post(`https://wjhtk.rf.gd/${process.env.NEXT_PUBLIC_API_KEY_REVEALINGTHENAMEOFTHECOUNTRY}`);
+    return response.data;
+  }
+);
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    nameLocal: "https://wjhtk.rf.gd",
+    name_Local_FrontEnd: "https://wjghtk-next-git-main-ahmedpahr9gmailcoms-projects.vercel.app",
+    value: 0,
+    CountryData: null, // Initialize as null
+
+  },
+  reducers: {
+
+
+    changenameLocal: (state, action) => {
+      state.nameLocal = action.payload;
+    },
+
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountryData.fulfilled, (state, action) => {
+      state.CountryData = action.payload; // Update the state with fetched data
+    });
+  },
+
+
+
+
+});
+
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  changeStateToken
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
